1cd3f2cc3a460fb365b8a1e3c62a1ec716a60661
TheGreatKO <hanlonko@gmail.com>
2016-10-08 06:42:34 -0400

    This version added Delete and Upate functionality to the People dialog and People Table.

--- cnbcPeopleAdminForm.py
+++ cnbcPeopleAdminForm.py
@@ -75,7 +75,7 @@ class PeopleAdmin(Ui_FormPeopleAdministration, QDialog):
             retval = msg.exec_ ( )
             return
 
-        if len ( self.lineEditRole.text ( ) ) == 0:
+        if self.comboBoxRole.currentText() == '':
             # If no Role provided - notify user
             logger.debug ( 'Error in adding a new Record - No role provided' )
             # build dialog
@@ -96,19 +96,62 @@ class PeopleAdmin(Ui_FormPeopleAdministration, QDialog):
             with db_session:
                 # Log the action
                 nm = self.lineEditName.text ( )
-                rl = self.lineEditRole.text ( )
+                rl = self.comboBoxRole.currentText()
                 s = 'Added the Name: {} with a Role: {} to the People Table'.format ( nm, rl )
                 logger.debug ( s )
 
                 # Add the record
                 p = People ( name=nm, role=rl )
+                # Update the table
                 self.buildTable ( )
 
         if self.radioButtonDelete.isChecked():
-            pass
+            # Delete record Action
+            with db_session:
+                # Log the action
+                nm = self.lineEditName.text ( )
+                rl = self.comboBoxRole.currentText()
+                s = 'Deleted the Name: {} with a Role: {} from the People Table'.format ( nm, rl )
+                logger.debug ( s )
+
+                # Delete the record
+                p = People ( name=nm, role=rl )
+                delete ( p for p in People if p.name == nm and p.role == rl)
+                # Repopulate name and role lineEdit controls
+                row = 0
+                c = self.tableWidgetPeople.item ( row, 0)
+                n = self.tableWidgetPeople.item ( row, 1 )
+                r = self.tableWidgetPeople.item ( row, 2 )
+                self.lineEditName.setText ( n.text ( ) )
+                self.comboBoxRole.setCurrentText(r.text())
+                self.lineEditID.setText ( c.text() )
+                # Update the table
+                self.buildTable ( )
 
         if self.radioButtonEdit.isChecked():
-            pass
+            # Edit record Action
+            with db_session:
+                # Log the action
+                nm = self.lineEditName.text ( )
+                rl = self.comboBoxRole.currentText ( )
+                s = 'Edited the Name: {} with a Role: {} Record in People Table'.format ( nm, rl )
+                logger.debug ( s )
+
+                # Edit the record
+                peepsNum = int(self.lineEditID.text())
+                peeps = People[peepsNum]
+                peeps.set ( name= nm, role=rl )
+
+                # Repopulate name and role lineEdit controls
+                row = 0
+                c = self.tableWidgetPeople.item ( row, 0 )
+                n = self.tableWidgetPeople.item ( row, 1 )
+                r = self.tableWidgetPeople.item ( row, 2 )
+                self.lineEditName.setText ( n.text ( ) )
+                self.comboBoxRole.setCurrentText ( r.text ( ) )
+                self.lineEditID.setText ( c.text() )
+                # Update the table
+                self.buildTable ( )
 
     def startup(self):
         '''
@@ -129,18 +172,20 @@ class PeopleAdmin(Ui_FormPeopleAdministration, QDialog):
         :param column:
         :return:
         '''
-        n = self.tableWidgetPeople.item ( row, 0)
-        r = self.tableWidgetPeople.item ( row, 1)
+        c = self.tableWidgetPeople.item ( row, 0)
+        n = self.tableWidgetPeople.item ( row, 1)
+        r = self.tableWidgetPeople.item ( row, 2)
         self.lineEditName.setText(n.text())
-        self.lineEditRole.setText(r.text())
+        self.comboBoxRole.setCurrentText ( r.text ( ) )
+        self.lineEditID.setText(c.text())
 
     def buildTable(self):
         with db_session:
             # Set the Table
             TotalRowCount = count ( c for c in People )
             self.tableWidgetPeople.setRowCount ( TotalRowCount )
-            self.tableWidgetPeople.setColumnCount ( 2 )
-            self.tableWidgetPeople.setHorizontalHeaderLabels ( ['Name', 'Role'] )
+            self.tableWidgetPeople.setColumnCount ( 3 )
+            self.tableWidgetPeople.setHorizontalHeaderLabels ( ['ID', 'Name', 'Role'] )
             self.tableWidgetPeople.setStyleSheet ( "QTableView {selection-background-color: QColor ( 0, 0, 255, 127)}" )
             self.tableWidgetPeople.setSelectionBehavior ( QAbstractItemView.SelectRows )
             self.tableWidgetPeople.horizontalHeader ( ).setStretchLastSection ( True )
@@ -150,8 +195,7 @@ class PeopleAdmin(Ui_FormPeopleAdministration, QDialog):
             for p in range ( TotalRowCount ):
                 while row <= TotalRowCount:
                     for p in select ( p for p in People ):
-                        self.tableWidgetPeople.setItem ( row, 0, QTableWidgetItem ( p.name ) )
-                        self.tableWidgetPeople.setItem ( row, 1, QTableWidgetItem ( p.role ) )
+                        self.tableWidgetPeople.setItem ( row, 0, QTableWidgetItem ( str(p.id) ) )
+                        self.tableWidgetPeople.setItem ( row, 1, QTableWidgetItem ( p.name ) )
+                        self.tableWidgetPeople.setItem ( row, 2, QTableWidgetItem ( p.role ) )
                         row = row + 1
-
-
